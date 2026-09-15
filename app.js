e12224bebbba05c9b672f1dfb29778a72bfb8cf4
Rohit Sharma <rohitsharma902495@gmail.com>
2026-09-16 01:14:33 +0530

    fix express 5 route

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/", userRouter);
 
-app.all("*",(req,res,next) => {
+app.use((req,res,next) => {
    next(new ExpressError(404,"page Not Found!"));
 });
 
